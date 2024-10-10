78e62c192a3531636703652aa707657890ce2161
FabienBoscher <fabien.boscher@epitech.digital>
2024-10-10 22:18:38 +0200

    Add files via upload

---
+++ ex_06/ex_06.js
@@ -0,0 +1,35 @@
+particlesJS("particules_top1", {
+  particles: {
+    number: { value: 150 },
+    color: { value: "#12e1fc" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+  },
+});
+
+particlesJS("particules_top2", {
+  particles: {
+    number: { value: 150 },
+    color: { value: "#d608ff" },
+    shape: { type: "edge" },
+    opacity: { value: 0.3 },
+  },
+});
+
+particlesJS("particules_bottom1", {
+  particles: {
+    number: { value: 150 },
+    color: { value: "#0ff23c" },
+    shape: { type: "triangle" },
+    opacity: { value: 0.7 },
+  },
+});
+
+particlesJS("particules_bottom2", {
+  particles: {
+    number: { value:150 },
+    color: { value: "#fcff3b" },
+    shape: { type: "polygon" },
+    opacity: { value: 0.9 },
+  },
+});
